5cf7d4553b9b144232fde082d875f6a74598cdf8
“julichk” <julia01.smith01@gmail.com>
2023-06-08 13:11:40 +0300

    done

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+    return (
+      <header className="header">
+        <div className="header_content">
+          <div className="header_logo"></div>
+          <div className="header-text">
+            <div className="header-text_space"></div>
+            <p className="header-text_space blog-name">Blog name</p>
+            <div className="header-text_space"></div>
+          </div>
+          <div className="header_list">
+            <nav className="header_list_nav">
+              <div className="header_list_nav_item"></div>
+              <div className="header_list_nav_item"></div>
+              <div className="header_list_nav_item"></div>
+              <div className="header_list_nav_item"></div>
+              <div className="header_list_nav_item"></div>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+  }
+  export default Header;
+  
